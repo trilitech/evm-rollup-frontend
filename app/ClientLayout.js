aaa9325a650d8190ddcbc377ea909a42bbda8bb5
sashaaldrick <sasha@aldrick.dev>
2023-10-26 14:24:01 +0100

    working language selector without 'backend'

--- app/ClientLayout.js
+++ app/ClientLayout.js
@@ -1,12 +1,19 @@
 "use client"
 
+import { useState } from "react";
+import { LanguageContext } from "../contexts/LanguageContext";
 import { ThemeProvider } from "next-themes";
 import "../css/tailwind.css";
 
 export default function ClientLayout({ children }) {
+  const [language, setLanguage] = useState('EN');
+
   return (
-    <ThemeProvider attribute="class"a defaultTheme="dark">
-        { children }
+    <ThemeProvider attribute="class" defaultTheme="dark">
+      <LanguageContext.Provider value={{ language, setLanguage }} >
+        {children}
+      </LanguageContext.Provider>
     </ThemeProvider>
+
   );
 }
